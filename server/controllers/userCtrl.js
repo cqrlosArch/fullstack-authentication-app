526588d61623e060d2f494415aaf7e7e0503c260
Godm0de <Godm0de@github.io>
2021-09-03 20:12:07 +0200

    Create sign up in auth controllers

--- server/controllers/userCtrl.js
+++
@@ -1,7 +0,0 @@
-const User = require("../models/User");
-
-const updateUser = (req, res) => {
-    const user = req.body;
-
-    User.findById(req.local.id).then((user) => {});
-};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 //Get user
+
 router.get("/");
 
 //Update user
+
 router.put("/");
 
 module.exports = router;
